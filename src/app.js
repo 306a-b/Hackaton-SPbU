c4e4601ed6e956ce1297a76dbabee1f3e98b2a56
MadMed677 <madmed677@gmail.com>
2016-03-31 19:34:13 +0300

    Without nested store works fine

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React                    from 'react';
 import ReactDOM                 from 'react-dom';
 
-import configStore              from './engine/_store';
+import store                    from './engine/_store';
 // import Root                  from './engine/root.react';
 import Routes                   from './engine/_routes';
 
-const store                     = configStore();
 console.log('store: ', store);
 
 ReactDOM.render(<Routes store={ store } />, document.getElementById('root'));
