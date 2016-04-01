3dfb69a19164d00c3984d0d6dbea8412cbbe7e88
schv <evgeniy.orlov96@gmail.com>
2016-04-01 13:29:31 +0300

    added database models

---
+++ db/database.py
@@ -0,0 +1,38 @@
+from app import app, db
+
+
+class Offer(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    time = db.Column(db.String(100))
+    category = db.Column(db.foreign)
+    desc = db.Column(db.String(500))
+    geo = db.Column(db.String(20))
+    url = db.Column(db.String(40))
+    tag = db.Column(db.String(100))
+
+    def __init__(self, ID, name, time, category, desc, geo, url, tag):
+        self.ID = ID
+        self.name = name
+        self.time = time
+        self.category = category
+        self.desc = desc
+        self.geo = geo
+        self.url = url
+        self.tag = tag
+
+    def __repr__(self):
+        return  # json()
+
+
+class Category(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+
+    def __init__(self, ID, name):
+        self.ID = ID
+        self.name = name
+
+    def __repr__(self):
+        return  # json()
+
