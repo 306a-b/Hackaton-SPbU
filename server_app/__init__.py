37991d86e13747227426cd6f361772ae02964848
kmtvb <smv995@gmail.com>
2016-04-02 03:22:36 +0300

    make https to all routes

--- server_app/__init__.py
+++ server_app/__init__.py
@@ -3,16 +3,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+isHeroku = os.environ.get('HEROKU') is not None
+
 app = Flask(__name__, template_folder='..', static_folder='', static_url_path='')
 db = SQLAlchemy(app)
 cors = CORS(app) # ВСЕ ЗАПРОСЫ МОГУТ ИМЕТЬ CORS
+if isHeroku: # обязательный ssl только на heroku
+    from flask_sslify import SSLify
+    #print('ssl is on')
+    sslify = SSLify(app)
 
 # cors config
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-isHeroku = os.environ.get('HEROKU') is not None
-
+# db config
 if isHeroku:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 else:
